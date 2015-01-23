f89e1565551d9e14432729bcf3cbf7178cf98b3f
imadake398yen <imadake398yen@gmail.com>
2015-01-24 02:53:02 +0900

    Initial Commit

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	public float rotateSpeed = 5f;
+
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		transform.eulerAngles += new Vector3(0f, h * Time.deltaTime * rotateSpeed, 0f);
+	}
+}
---
+++ Assets/Scripts/StateMacine.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateMacine : MonoBehaviour {
+
+	public Transform other;
+
+	enum STATE {
+		ATTACK,
+		CARE,
+		SAFE
+	}
+	private STATE state = STATE.SAFE;
+	
+	void Start () {
+	}
+	
+	void Update () {
+		if (other) {
+
+			Vector3 forward = transform.TransformDirection(Vector3.forward);
+			Vector3 toOther = Vector3.Normalize(other.position - transform.position);
+			float dot = Vector3.Dot(forward, toOther);
+        	
+			if (dot > 0.8f) {
+				renderer.material.color = Color.red;
+			}
+			if (dot <= 0.8f && dot > 0.1f) {
+				renderer.material.color = Color.yellow;
+			}
+			if (dot <= 0.1f) {
+				renderer.material.color = Color.green;
+			}
+            
+        }
+	}
+}
