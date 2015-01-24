a93bb2ced7bcc904afab45cd55bf64c25fcc5247
imadake398yen <imadake398yen@gmail.com>
2015-01-24 13:23:09 +0900

    Add enum STATE

--- Assets/Scripts/StateMacine.cs
+++ Assets/Scripts/StateMacine.cs
@@ -16,22 +16,37 @@ public class StateMacine : MonoBehaviour {
 	}
 	
 	void Update () {
-		if (other) {
 
+		if (other) {
 			Vector3 forward = transform.TransformDirection(Vector3.forward);
 			Vector3 toOther = Vector3.Normalize(other.position - transform.position);
 			float dot = Vector3.Dot(forward, toOther);
         	
 			if (dot > 0.8f) {
 				renderer.material.color = Color.red;
+				state = STATE.ATTACK;
 			}
 			if (dot <= 0.8f && dot > 0.1f) {
 				renderer.material.color = Color.yellow;
+				state = STATE.CARE;
 			}
 			if (dot <= 0.1f) {
 				renderer.material.color = Color.green;
+				state = STATE.SAFE;
 			}
-            
         }
+
+        if (state == STATE.SAFE) {
+        	return;
+        }
+
+		if (state == STATE.CARE) {
+        	return;
+        }
+
+        if (state == STATE.ATTACK) {
+        	return;
+        }        
+
 	}
 }
